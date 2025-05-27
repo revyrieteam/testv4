b4d99d38572b44b6399ffc3c460670251c40030d
Revyrie DevOps <devops@revyrieglobal.com>
2025-05-28 00:21:35 +0530

    Update bug.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- bug.js
+++ bug.js
@@ -11,7 +11,7 @@ function sanitize(input) {
 
 app.get('/user/:id', function (req, res) {
     const userId = sanitize(req.params.id); // ✅ Sanitize input
-    res.send(`User ID is: ${sanitize(userId)}`);
+    res.send(`User ID is: ${sanitize(userId).replace(/[^0-9]/g, '')}`);
 });
 
 app.listen(3000, () => {
